14bd7f0db90d018d8c0c0b1e3e831f3197719a47
SuneDalsagerBro <sune.bro@ka-net.dk>
2020-01-18 11:12:39 +0100

    Fixed errors

--- Pong.py
+++ Pong.py
@@ -1,5 +1,4 @@
 import turtle
-from _tkinter import *
 
 vn = turtle.Screen()
 vn.title("pong")
@@ -35,9 +34,10 @@ ball.speed(0)
 ball.shape("square")
 ball.color("white")
 ball.penup()
-ball.goto(-0, 0)
+ball.goto(0, 0)
 ball.dx = 2
 ball.dy = 2
+
 
 # pen
 # pen = turtle.Turtle
@@ -81,10 +81,10 @@ def paddle_b_down():
 
 
 vn.listen()
-vn.onkeypress(paddle_a_up(), "w")
-vn.onkeypress(paddle_a_down(), "s")
-vn.onkeypress(paddle_b_up(), "Up")
-vn.onkeypress(paddle_a_down(), "Down")
+vn.onkeypress(paddle_a_up, "w")
+vn.onkeypress(paddle_a_down, "s")
+vn.onkeypress(paddle_b_up, "Up")
+vn.onkeypress(paddle_b_down, "Down")
 
 while True:
     vn.update()
@@ -96,30 +96,23 @@ while True:
         ball.sety(290)
         ball.dy *= -1
 
-    if ball.ycor() < -290:
+    elif ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
 
-        if ball.xcor() > 390:
+        if ball.xcor() > 350:
             ball.goto(0, 0)
             ball.dx *= -1
-            score_a += 1
-            # pen.clear()
-            # pen.write("Player A : {} Player B : {}".format(score_a, score_b), align="center",
-            # font=("Courier", 24, "normal"))
-            # help
 
-            if ball.xcor() < -390:
-                ball.goto(0, 0)
-                ball.dx *= -1
-                score_b += 1
-                # pen.clear()
-                # pen.write("Player A : {} Player B : {}".format(score_a, score_b), align="center",
-                # font=("Courier", 24, "normal"))
+
+
+        elif ball.xcor() < -350:
+            ball.goto(0, 0)
+            ball.dx *= -1
 
             if 340 < ball.xcor() < 350 and (paddle_b.ycor() + 40 > ball.ycor() > paddle_b.ycor() - 50):
                 ball.setx(340)
-                ball.dx *= -1
+            ball.dx *= -1
 
             if 340 < ball.xcor() < 350 and (paddle_a.ycor() + 40 > ball.ycor() > paddle_a.ycor() - 50):
                 ball.setx(-340)
