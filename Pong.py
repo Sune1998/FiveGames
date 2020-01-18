fe1f83296b4c7487bbc57b6212dfd61c42c53314
SuneDalsagerBro <sune.bro@ka-net.dk>
2020-01-18 11:57:50 +0100

    Changde stuff

--- Pong.py
+++ Pong.py
@@ -1,16 +1,12 @@
 import turtle
 
-vn = turtle.Screen()
-vn.title("pong")
-vn.bgcolor("black")
-vn.setup(width=800, height=600)
-vn.tracer(0)
+wn = turtle.Screen()
+wn.title("pong")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
 
-# score
-
-score_a = 0
-score_b = 0
-# paddle A
+# paddle a
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
@@ -19,7 +15,7 @@ paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
 
-# paddle B
+# paddle b
 paddle_b = turtle.Turtle()
 paddle_b.speed(0)
 paddle_b.shape("square")
@@ -28,27 +24,13 @@ paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(350, 0)
 
-# Ball
+# paddle a
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
-ball.dx = 2
-ball.dy = 2
-
-
-# pen
-# pen = turtle.Turtle
-
-
-# pen.speed(0)
-# pen.color('white')
-# pen.penup()
-# pen.hideturtle()
-# pen.goto(0, 260)
-# pen.write("Player A : 0 Player B : 0", align="center", font=("Courier", 24, "normal"))
 
 
 # Function
@@ -57,63 +39,29 @@ def paddle_a_up():
     y += 20
     paddle_a.sety(y)
 
+    def paddle_a_down():
+        y = paddle_a.ycor()
+        y -= 20
+        paddle_a.sety(y)
 
-def paddle_a_down():
-    y = paddle_a.ycor()
-    y -= 20
-    paddle_a.sety(y)
+    def paddle_b_up():
+        y = paddle_a.ycor()
+        y += 20
+        paddle_a.sety(y)
 
-    # Function
+    def paddle_b_down():
+        y = paddle_b.ycor()
+        y -= 20
+        paddle_b.sety(y)
 
-
-def paddle_b_up():
-    y = paddle_b.ycor()
-    y += 20
-    paddle_b.sety(y)
-
-
-def paddle_b_down():
-    y = paddle_b.ycor()
-    y -= 20
-    paddle_b.sety(y)
-
-    # Keyboard Binding
+    # keyboardbinding
+    wn.listen()
+    wn.onkeypress(paddle_a_up(), "w")
+    wn.onkeypress(paddle_a_down(), "s")
+    wn.onkeypress(paddle_b_up(), "Up")
+    wn.onkeypress(paddle_b_down(), "Down")
 
 
-vn.listen()
-vn.onkeypress(paddle_a_up, "w")
-vn.onkeypress(paddle_a_down, "s")
-vn.onkeypress(paddle_b_up, "Up")
-vn.onkeypress(paddle_b_down, "Down")
-
+# main game loop
 while True:
-    vn.update()
-    ball.setx(ball.xcor() + ball.dx)
-    ball.sety(ball.ycor() + ball.dy)
-
-    # Border Checking
-    if ball.ycor() > 290:
-        ball.sety(290)
-        ball.dy *= -1
-
-    elif ball.ycor() < -290:
-        ball.sety(-290)
-        ball.dy *= -1
-
-        if ball.xcor() > 350:
-            ball.goto(0, 0)
-            ball.dx *= -1
-
-
-
-        elif ball.xcor() < -350:
-            ball.goto(0, 0)
-            ball.dx *= -1
-
-            if 340 < ball.xcor() < 350 and (paddle_b.ycor() + 40 > ball.ycor() > paddle_b.ycor() - 50):
-                ball.setx(340)
-            ball.dx *= -1
-
-            if 340 < ball.xcor() < 350 and (paddle_a.ycor() + 40 > ball.ycor() > paddle_a.ycor() - 50):
-                ball.setx(-340)
-                ball.dx *= -1
+    wn.update()
