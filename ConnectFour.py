6016dfd42e40edd9f002d37b6664ed9e8d0a757a
sune <sune.bro@ka-net.dk>
2020-01-27 14:25:06 +0100

    Added Display window and controller logic

--- ConnectFour.py
+++ ConnectFour.py
@@ -41,25 +41,25 @@ def winning_move(board, piece):
     for c in range(COLUMN_COUNT - 3):
         for r in range(ROW_COUNT):
             if board[r][c] == piece and board[r][c + 1] == piece and board[r][c + 2] == piece and board[r][
-                 c + 3] == piece:
+                c + 3] == piece:
                 return True
 
     for c in range(COLUMN_COUNT):
         for r in range(ROW_COUNT - 3):
             if board[r][c] == piece and board[r + 1][c] == piece and board[r + 2][c] == piece and board[r + 3][
-                 c] == piece:
+                c] == piece:
                 return True
 
     for c in range(COLUMN_COUNT - 3):
         for r in range(ROW_COUNT - 3):
             if board[r][c] == piece and board[r + 1][c + 1] == piece and board[r + 2][c + 2] == piece and board[r + 3][
-                 c + 3] == piece:
+                c + 3] == piece:
                 return True
 
     for c in range(COLUMN_COUNT - 3):
         for r in range(3, ROW_COUNT):
             if board[r][c] == piece and board[r - 1][c + 1] == piece and board[r - 2][c + 2] == piece and board[r - 3][
-                 c + 3] == piece:
+                c + 3] == piece:
                 return True
 
 
@@ -83,6 +83,13 @@ def draw_board(board):
     pygame.display.update()
 
 
+board = create_board()
+print_board(board)
+game_over = False
+turn = 0
+
+pygame.init()
+
 SQUARESIZE = 100
 
 width = COLUMN_COUNT * SQUARESIZE
@@ -91,3 +98,19 @@ hight = (ROW_COUNT + 1) * SQUARESIZE
 size = (width, hight)
 
 RADIUS = int(SQUARESIZE / 2 - 5)
+
+screen = pygame.display.set_mode(size)
+draw_board(board)
+pygame.display.update()
+
+myfont = pygame.font.SysFont("monospace", 75)
+
+while not game_over:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+        if event.type == pygame.MOUSEMOTION:
+            pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
+            posx = event.pos[0]
